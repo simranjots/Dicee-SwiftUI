e94fa26c42bd61d21ca91f6bcea620d6e4c64d30
simranjot singh <32786222+simranjots@users.noreply.github.com>
2021-08-06 02:05:53 -0400

    Initial Commit

---
+++ Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Simranjot Singh Bagga on 2021-08-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
